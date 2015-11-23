193a01070ad0023032cdb6fd989ecae764736b60
Mindy Mallory <mindy.mallory@gmail.com>
2015-11-23 16:01:59 -0600

    fixed the site.url issue

--- _drafts/r2jekyll.R
+++ _drafts/r2jekyll.R
@@ -1,9 +1,15 @@
-#!/usr/bin/env Rscript
+# This script takes the .Rmd document and converts it to a .md document 
+# that can be used by Jekyll/Github Pages to build a website. Change 
+# filename to the desired one in the '~/_drafts' folder. 
+# Modified from Nicole White's blog post on blogging with rmarkdown and Jekyll: 
+# http://nicolewhite.github.io/2015/02/07/r-blogging-with-rmarkdown-knitr-jekyll.html
+
+
 library(knitr)
 
 # Get the filename given as an argument in the shell.
-args = commandArgs(TRUE)
-filename = args[1]
+#args = commandArgs(TRUE)
+filename = 'test2.Rmd'
 
 # Check that it's a .Rmd file.
 if(!grepl(".Rmd", filename)) {
@@ -11,14 +17,8 @@ if(!grepl(".Rmd", filename)) {
 }
 
 # Knit and place in _posts.
-dir = paste0("../_posts/", Sys.Date(), "-")
+dir = paste0("_posts/", Sys.Date(), "-")
 output = paste0(dir, sub('.Rmd', '.md', filename))
-knit(filename, output)
+knit(paste0("_drafts/",filename), output)
 
-# Copy .png files to the images directory.
-fromdir = "{{ site.url }}/images"
-todir = "../images"
 
-pics = list.files(fromdir, ".png")
-pics = sapply(pics, function(x) paste(fromdir, x, sep="/"))
-file.copy(pics, todir)
